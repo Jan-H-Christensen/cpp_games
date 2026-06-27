35334d77c79c73f602f0e74392e517b62ccc0d8f
Jan-H-Christensen <jan.christensen1981@gmail.com>
2026-06-27 12:56:10 +0100

    init cellular automata

---
+++ Elementary_CA/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+RAYLIB_PATH="/home/jc/Documents/GameDev/raylib/src"
+includes="-Iinclude -I$RAYLIB_PATH"
+defines=""
+warnings="-Wno-writable-strings -Wno-format-security"
+
+if [[ "$(uname)" == "Linux" ]]; then
+    echo "Building for Linux..."
+    libs="-L$RAYLIB_PATH -lraylib -lGL -lm -lpthread -ldl -lrt -lX11"
+    outputFile="bin/main"
+
+elif [[ "$(uname)" == "Darwin" ]]; then
+    echo "Building for macOS..."
+    libs="-L$RAYLIB_PATH -lraylib -lGL -lm"
+    outputFile="bin/main"
+
+else
+    echo "Building for Windows..."
+    libs="-L$RAYLIB_PATH -lraylib -luser32 -lopengl32 -lgdi32"
+    outputFile="bin/main.exe"
+fi
+
+mkdir -p bin
+clang++ -std=c++17 -g $includes $defines $warnings src/main.cpp -o $outputFile $libs
--- Elementary_CA/src/main.cpp
+++ Elementary_CA/src/main.cpp
@@ -16,6 +16,11 @@ int main() {
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawText("raylib und C++ laufen!", 10, 10, 20, DARKGRAY);
+    for (int i = 0; i < screenWidth / 5; i++) {
+      for (int j = 0; j < screenHeight / 5; j++) {
+        DrawRectangleLines(i * 5, j * 5, 5, 5, BLUE);
+      }
+    }
     EndDrawing();
   }
 
